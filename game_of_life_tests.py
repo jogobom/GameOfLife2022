ab02847bc9725727321da18d669dddb268f30df4
Chris Preston <Chris_Preston@waters.com>
2022-06-28 13:30:08 +0100

    Use hypothesis for testing rules

--- game_of_life_tests.py
+++ game_of_life_tests.py
@@ -1,6 +1,8 @@
 import unittest
 from assertpy import assert_that
 from game_of_life_rules import should_live
+from hypothesis import given
+from hypothesis.strategies import integers
 
 
 def count_live_neighbours(grid):
@@ -16,10 +18,9 @@ class GameOfLifeRules(unittest.TestCase):
     def test_cell_with_3_live_neighbours_lives():
         assert_that(should_live(3)).is_true()
 
-    @staticmethod
-    def test_cell_with_more_than_3_live_neighbours_lives():
-        for i in range(4, 10):
-            assert_that(should_live(i)).is_false()
+    @given(integers(min_value=4))
+    def test_cell_with_more_than_3_live_neighbours_lives(self, i):
+        assert_that(should_live(i)).is_false()
 
 
 if __name__ == '__main__':
